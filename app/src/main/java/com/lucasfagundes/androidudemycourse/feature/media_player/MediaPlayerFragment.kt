c04dff33adece7723adca2508c56d0ea13b997a4
lucas <lukasfcarneiro@gmail.com>
2021-03-25 17:22:33 -0300

    Media player volume control added

--- app/src/main/java/com/lucasfagundes/androidudemycourse/feature/media_player/MediaPlayerFragment.kt
+++ app/src/main/java/com/lucasfagundes/androidudemycourse/feature/media_player/MediaPlayerFragment.kt
@@ -1,11 +1,14 @@
 package com.lucasfagundes.androidudemycourse.feature.media_player
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import com.lucasfagundes.androidudemycourse.R
 import com.lucasfagundes.androidudemycourse.databinding.FragmentMediaPlayerBinding
 
@@ -13,11 +16,13 @@ class MediaPlayerFragment : Fragment() {
 
     private lateinit var binding: FragmentMediaPlayerBinding
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var audioManager: AudioManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
+
         binding = FragmentMediaPlayerBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -26,6 +31,7 @@ class MediaPlayerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         mediaPlayer = MediaPlayer.create(context, R.raw.teste)
+        initializeSeekBar()
 
         binding.playButton.setOnClickListener() {
             playSound()
@@ -36,7 +42,39 @@ class MediaPlayerFragment : Fragment() {
         binding.stopButton.setOnClickListener() {
             stopSound()
         }
-    }//fun
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+    }
+
+    private fun initializeSeekBar() {
+        audioManager =
+            activity?.applicationContext?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        binding.volumeSeekBar.max = maxVolume
+        binding.volumeSeekBar.progress = currentVolume
+
+        binding.volumeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
+                binding.volumeNumberTextView.text = getString(R.string.percent, progress.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+    }
 
     private fun playSound() {
         mediaPlayer.start()
@@ -51,8 +89,8 @@ class MediaPlayerFragment : Fragment() {
     private fun stopSound() {
         if (mediaPlayer.isPlaying) {
             mediaPlayer.stop()
-            mediaPlayer = MediaPlayer.create(context, R.raw.teste)
         }
+        mediaPlayer = MediaPlayer.create(context, R.raw.teste)
     }
 }
 
